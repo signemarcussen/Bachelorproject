f428a6252bb2828344638afead0912621e75b9b7
signemarcussen <engis-10@hotmail.com>
2021-08-30 16:31:32 +0200

    load data

--- Rscripts/01_load.R
+++ Rscripts/01_load.R
@@ -10,10 +10,27 @@ library("tidyverse")
 
 
 # Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = ".tsv")
+subject_metadata <- read_csv(file = "data/_raw/subject-metadata.csv")
+peptide_detail_ci <- read_csv(file = "data/_raw/peptide-detail-ci.csv")
+peptide_detail_cii <- read_csv(file = "data/_raw/peptide-detail-cii.csv")
+minigene_detail <- read_csv(file = "data/_raw/minigene-detail.csv")
+orfs <- read_csv(file = "data/_raw/orfs.csv")
+peptide_hits_ci <- read_csv(file = "data/_raw/peptide-hits-ci.csv")
+peptide_hits_cii <- read_csv(file = "data/_raw/peptide-hits-cii.csv")
+minigene_hits <- read_csv(file = "data/_raw/minigene-hits.csv")
 
 
 # Wrangle data ------------------------------------------------------------
+
+# Combine peptide C1 and C2 experiments, and join subject_metadata
+dat <- rbind(peptide_detail_ci, 
+             peptide_detail_cii) %>% 
+      full_join(x = ., 
+                y = subject_metadata, 
+                by = "Experiment")
+
+
+
 my_data_subset <- my_data %>% 
       filter(...) %>% 
       select(...) %>% 
