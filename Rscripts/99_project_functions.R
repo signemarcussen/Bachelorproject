32dd3eeaa2b3c3d80644ced1eb917e1496b5fb44
signemarcussen <engis-10@hotmail.com>
2021-09-17 13:40:43 +0200

    blosum function shell

--- Rscripts/99_project_functions.R
+++ Rscripts/99_project_functions.R
@@ -1,3 +1,7 @@
 # Define project functions ------------------------------------------------
 
-
+## Make numerical representation of peptides/proteins using BLOSUM-X 
+## encoding. 
+blosum_encoding <- function(peptides, blosum_matrix) {
+      
+}
