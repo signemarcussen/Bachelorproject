d08a1f6544f752de03197f062e4b1546a54806f1
signemarcussen <engis-10@hotmail.com>
2021-09-20 11:35:04 +0200

    change row/col

--- Rscripts/99_project_functions.R
+++ Rscripts/99_project_functions.R
@@ -12,7 +12,7 @@ blosum_encoding <- function(x, m) {
       X_enc <- lapply(strsplit(x, split = ""),
                       function(x_i) { return( c(m[,x_i]) ) })
       X_enc <- do.call(rbind, X_enc)
-      colnames(X_enc) <- x
-      rownames(X_enc) <- rep(colnames(m), nchar(x[1]))
+      rownames(X_enc) <- x
+      colnames(X_enc) <- rep(colnames(m), nchar(x[1]))
       return(X_enc)
 }
