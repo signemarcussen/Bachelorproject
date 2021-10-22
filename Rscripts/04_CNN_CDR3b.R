74611b5cf76efff3b5ea404929e0d181428db2c8
LucilleAugusta <s184250@student.dtu.dk>
2021-10-22 12:09:10 +0200

    opdater cdr3b encoding

--- Rscripts/04_CNN_CDR3b.R
+++ Rscripts/04_CNN_CDR3b.R
@@ -29,15 +29,22 @@ blosum62 <- rbind(blosum62, X)
 
 ## Define training/test set
 set.seed(2005)
+data_A0201 <- data_A0201 %>%
+   sample_n(10000)
+
 data_A0201_Xy <- data_A0201 %>% 
       mutate(Set = sample(c("train", "test"),
                           size = nrow(.),
                           replace = TRUE,
                           prob = c(0.8, 0.2))) %>% 
+            # Partition = sample(1:5,
+             #                   size = nrow(.),
+              #                  replace = TRUE,
+               #                 prob = c(0.2, 0.2, 0.2, 0.2, 0.2))) %>% 
    drop_na()
 
 #Padding short sequences with "X"
-pad_data_A0201_Xy <- data_A0201_Xy %>% 
+data_A0201_Xy <- data_A0201_Xy %>% 
    mutate(CDR3b = str_pad(string = CDR3b, 
                           width = max(nchar(CDR3b)), 
                           side="right", pad="X"))
@@ -50,26 +57,21 @@ data_A0201_Xy %>%
 ### Using pep_encode, to create 3D tensor of the CDR3b sequences
 ### with dimensions n_peps x l_peps x l_enc, this is converted to a 1D array. 
 ### so the first l_peps x l_enc entries correspond to one seq.
-
-X_train <- pad_data_A0201_Xy %>% 
+max_seq <- data_A0201_Xy %>% select(CDR3b_size) %>%  max(.)
+   
+X_train <- data_A0201_Xy %>% 
       filter(Set == "train") %>% 
       pull(CDR3b)%>% 
       pep_encode(pep = .) %>% 
-      array_reshape(., c(nrow(.), 25, 21, 1))
+      array_reshape(., c(nrow(.), max_seq, 21, 1))
 #Before reshape: 
 #dim(X_train) = 158396 -seqs     25-lenght of seqs    21-cols in Blosum62
 
 X_test <- data_A0201_Xy %>% 
    filter(Set == "test") %>% 
-   pull(Peptide) %>% 
-   pep_encode_Signe(pep = ., matrix = blosum62) %>% 
-   array_reshape(., c(nrow(.), 9, 20, 1))
-
-X_test <- data_A0201_Xy %>% 
-      filter(Set == "test") %>% 
-      pull(CDR3b) %>% 
-      blosum_encoding(x = .,
-                      m = blosum62)
+   pull(CDR3b)%>% 
+   pep_encode(pep = .) %>% 
+   array_reshape(., c(nrow(.), max_seq, 21, 1))
 
 y_train <- data_A0201_Xy %>% 
       filter(Set == "train") %>% 
@@ -79,6 +81,52 @@ y_test <- data_A0201_Xy %>%
       filter(Set == "test") %>% 
       pull(Binding) 
 
+
+# Model data --------------------------------------------------------------
+
+## Set hyperparameters
+n_epochs <- 5 #300 / 50
+batch_size <- 500
+loss_func <- "binary_crossentropy"
+learn_rate <- 0.001
+input_shape <- c(max_seq, 21, 1)
+
+## Build model architecture
+cnn_model <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 32,
+                 kernel_size = c(3, 3),
+                 activation = 'relu',
+                 input_shape = input_shape) %>%
+   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+   layer_conv_2d(filters = 64,
+                 kernel_size = c(3, 3),
+                 activation = 'relu') %>%
+   #layer_dropout(rate = 0.25) %>% 
+   layer_flatten() %>% 
+   #layer_dense(units  = 180, activation = 'relu') %>% 
+   #layer_dropout(rate = 0.4) %>% 
+   #layer_dense(units  = 10, activation  = 'relu') %>%
+   #layer_dropout(rate = 0.3) %>%
+   layer_dense(units = 10, activation = "relu") %>% 
+   layer_dense(units  = 1, activation   = 'sigmoid')
+
+## Compile model
+cnn_model %>% 
+   compile(loss = loss_func,
+           optimizer = optimizer_rmsprop(learning_rate = learn_rate),
+           metrics = "accuracy")
+
+## View model
+cnn_model %>% summary()
+
+
+## Train model
+cnn_history <- cnn_model %>% 
+   fit(x = X_train,
+       y = y_train,
+       epochs = n_epochs,
+       batch_size = batch_size,
+       validation_split = 0.2)
 
 # Model data --------------------------------------------------------------
 
