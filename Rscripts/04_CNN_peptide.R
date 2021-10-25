0550f1975ddd856f7eb44004cd8feef06abe4d55
signemarcussen <engis-10@hotmail.com>
2021-10-25 10:18:23 +0200

    try layer with diff. kernel size

--- Rscripts/04_CNN_peptide.R
+++ Rscripts/04_CNN_peptide.R
@@ -15,7 +15,7 @@ source("Rscripts/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-data_A0201 <- read_tsv(file = "data/03_data_complete_A0201.tsv.gz")
+data_A0201 <- read_tsv(file = "data/03_data_A0201_complete.tsv.gz")
 blosum62_raw <- read.table(file = "data/_raw/BLOSUM62.txt", 
                            skip = 6)
 
@@ -29,7 +29,7 @@ blosum62 <- blosum62_raw %>%
 
 ## Define training/test set
 set.seed(2005)
-data_A0201 <- data_A0201 %>%
+data_A0201 <- data_A0201 %>% # SUBSET
    sample_n(10000)
 
 data_A0201_Xy <- data_A0201 %>% 
@@ -46,33 +46,32 @@ data_A0201_Xy %>%
 X_train <- data_A0201_Xy %>% 
    filter(Set == "train") %>% 
    pull(Peptide) %>% 
-   pep_encode_Signe(pep = ., matrix = blosum62) %>% 
+   blosum_encoding(peptide = ., 
+                   blosum_matrix = blosum62) %>% 
    array_reshape(., c(nrow(.), 9, 20, 1))
-#skal vi ikke bare bruge den oprindlige pep_encode?
 
 X_test <- data_A0201_Xy %>% 
    filter(Set == "test") %>% 
    pull(Peptide) %>% 
-   pep_encode_Signe(pep = ., matrix = blosum62) %>% 
+   blosum_encoding(peptide = ., 
+                   blosum_matrix = blosum62) %>% 
    array_reshape(., c(nrow(.), 9, 20, 1))
 
 y_train <- data_A0201_Xy %>% 
    filter(Set == "train") %>% 
-    pull(Binding)
+   pull(Binding)
 
 y_test <- data_A0201_Xy %>% 
    filter(Set == "test") %>% 
    pull(Binding)
 
 
-## 
-
 
 # Model data --------------------------------------------------------------
 
 ## Set hyperparameters
 n_epochs <- 5 #300 / 50
-batch_size <- 500
+batch_size <- 128 #128
 loss_func <- "binary_crossentropy"
 learn_rate <- 0.001
 input_shape <- c(9, 20, 1)
@@ -80,22 +79,81 @@ input_shape <- c(9, 20, 1)
 ## Build model architecture
 cnn_model <- keras_model_sequential() %>% 
    layer_conv_2d(filters = 32,
-                 kernel_size = c(3, 3),
+                 kernel_size = 3,
                  activation = 'relu',
                  input_shape = input_shape) %>%
    layer_max_pooling_2d(pool_size = c(2,2)) %>% 
-   layer_conv_2d(filters = 64,
-                 kernel_size = c(3, 3),
-                 activation = 'relu') %>%
    #layer_dropout(rate = 0.25) %>% 
    layer_flatten() %>% 
-   #layer_dense(units  = 180, activation = 'relu') %>% 
-   #layer_dropout(rate = 0.4) %>% 
-   #layer_dense(units  = 10, activation  = 'relu') %>%
-   #layer_dropout(rate = 0.3) %>%
-   layer_dense(units = 10, activation = "relu") %>% 
+   layer_dense(units = 32, activation = "relu") %>% 
    layer_dense(units  = 1, activation   = 'sigmoid')
-    
+
+
+## Model with several kernel_sizes (Not working)
+conv_l1_k1 <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 16,
+                 kernel_size = 1,
+                 padding = "same",
+                 activation = 'relu',
+                 input_shape = input_shape) %>% 
+   layer_max_pooling_2d(pool_size = c(2,2))
+conv_l1_k3 <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 16,
+                 kernel_size = 3,
+                 padding = "same",
+                 activation = 'relu',
+                 input_shape = input_shape) %>%
+   layer_max_pooling_2d(pool_size = c(2,2))
+conv_l1_k5 <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 16,
+                 kernel_size = 5,
+                 padding = "same",
+                 activation = 'relu',
+                 input_shape = input_shape) %>%
+   layer_max_pooling_2d(pool_size = c(2,2))
+conv_l1_k7 <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 16,
+                 kernel_size = 7,
+                 padding = "same",
+                 activation = 'relu',
+                 input_shape = input_shape) %>%
+   layer_max_pooling_2d(pool_size = c(2,2))
+conv_l1_k9 <- keras_model_sequential() %>% 
+   layer_conv_2d(filters = 16,
+                 kernel_size = 9,
+                 padding = "same",
+                 activation = 'relu',
+                 input_shape = input_shape) 
+
+predictions <- layer_concatenate(inputs = c(conv_l1_k1, 
+                                            conv_l1_k3, 
+                                            conv_l1_k5, 
+                                            conv_l1_k7, 
+                                            conv_l1_k9)) %>% 
+   layer_dense(units = 32, activation = "relu") %>% 
+   layer_dense(units  = 1, activation   = 'sigmoid')
+
+cnn_model <- keras_model(inputs = input_shape,
+                         outputs = predictions)
+## Another try: (Read up on functional API: https://keras.rstudio.com/articles/functional_api.html)
+conv_k1 <- layer_conv_2d(filters = 16,
+                            kernel_size = 1,
+                            padding = "same",
+                            activation = 'relu',
+                            input_shape = input_shape) 
+
+pool_k1 <- layer_max_pooling_2d(object = conv_k1, pool_size = c(2,2))
+
+input <- layer_input(shape = input_shape)
+model_out <- 
+
+model <- keras_model(
+   inputs = c(),
+   outputs = c()
+)
+
+
+
 ## Compile model
 cnn_model %>% 
    compile(loss = loss_func,
