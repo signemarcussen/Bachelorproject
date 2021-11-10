195057aae7ff7f0c5b695472668faf3b9ec49f3e
LucilleAugusta <s184250@student.dtu.dk>
2021-11-10 11:40:20 +0100

    cc

--- Rscripts/04_CNN_onehot.R
+++ Rscripts/04_CNN_onehot.R
@@ -28,6 +28,26 @@ X_pep <- data_A0201 %>%
 
 aa <- c('L','I','T','S','F','A','N','M','P','G','K','Q','Y','V','H','W','D','E','R','C')
 # Model data --------------------------------------------------------------
-
+dcast(data = data, Outcome ~ Variable, length)
 
 # Write data --------------------------------------------------------------
+
+
+
+dt <- data.frame(
+      ID=1:5, 
+      Color=factor(c("green", "red", "red", "blue", "green"), levels=c("blue", "green", "red", "purple")),
+      Shape=factor(c("square", "triangle", "square", "triangle", "cirlce"))
+)
+
+dt
+
+# ID Color    Shape
+# 1  1 green   square
+# 2  2   red triangle
+# 3  3   red   square
+# 4  4  blue triangle
+# 5  5 green   cirlce
+
+# one hot encode the colors
+dcast(melt(dt, id.vars='ID'), ID ~ variable + value, fun = length)
