fd465638a337a94003bb6fa5dd978a5c817ce34c
LucilleAugusta <s184250@student.dtu.dk>
2021-11-11 11:50:46 +0100

    one hot

--- Rscripts/04_CNN_onehot.R
+++ Rscripts/04_CNN_onehot.R
@@ -23,30 +23,44 @@ data_A0201 <- data_A0201 %>% # SUBSET
 
 # Wrangle data ------------------------------------------------------------
 # One-hot encoding 
-X_pep <- data_A0201 %>%  
-      pull(Peptide)
+one_hot_encoding <- function(sequence) {
+   
+   amino_acids = "LITSFANMPGKQYVHWDERC"
+   
+   # Define encoding matrix
+   m = nchar(amino_acids)
+   n = nchar(amino_acids)
+   enc = diag(1, m, n)
+   
+   dimnames(enc) = c(strsplit(amino_acids, ""),
+                     strsplit(amino_acids, ""))
+   
+   # Encode the sequence 
+   x_enc = enc[unlist(strsplit(sequence, "")),]
+   
+   return(x_enc)
+   
+}
 
-aa <- c('L','I','T','S','F','A','N','M','P','G','K','Q','Y','V','H','W','D','E','R','C')
+encode <- data_A0201 %>% 
+   pull(Peptide) %>% 
+   one_hot_encoding(.) %>% 
+   as.array(.)
+encode
+
+%>% 
+   array_reshape(., c(nrow(.), 9, 20, 1))
 # Model data --------------------------------------------------------------
-dcast(data = data, Outcome ~ Variable, length)
 
 # Write data --------------------------------------------------------------
 
 
 
-dt <- data.frame(
-      ID=c(1,1:5), 
-      Color=c("green","green", "red", "red", "blue", "green"),
-      Shape=as.character(c("square","square", "triangle", "square", "triangle", "cirlce")))
 
-dt
 
-# ID Color    Shape
-# 1  1 green   square
-# 2  2   red triangle
-# 3  3   red   square
-# 4  4  blue triangle
-# 5  5 green   cirlce
 
-# one hot encode the colors
-dcast(melt(dt, id.vars='ID'), ID ~ variable + value, fun = length)
+
+
+
+
+
