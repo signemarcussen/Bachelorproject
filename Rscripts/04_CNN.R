d1bdc046e02c65fddf97c08a15b40702cedba585
signemarcussen <engis-10@hotmail.com>
2021-11-14 21:13:21 +0100

    -

--- Rscripts/04_CNN.R
+++ Rscripts/04_CNN.R
@@ -71,6 +71,7 @@ data_A0201 %>% count(Binding)
 
 
 ## 5-fold nested cross-validation
+set.seed(789)
 partitions <- data_A0201 %>% 
       nested_cv(outside = vfold_cv(v = 5), 
                 inside = vfold_cv(v = 4))
@@ -98,123 +99,6 @@ learn_rate <- 0.001
 input_shape_pep <- c(9, 20, 1)
 input_shape_CDR3b <- c(max_CDR3b, 21, 1)
 
-## Build peptide layers
-peptide_input <- layer_input(shape = input_shape_pep)
-
-pep_k1 <- peptide_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(1, 20),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_pep) %>% 
-      layer_max_pooling_2d(pool_size = 2)
-pep_k3 <- peptide_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(3, 20),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_pep) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-pep_k5 <- peptide_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(5, 20),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_pep) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-pep_k7 <- peptide_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(7, 20),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_pep) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-pep_k9 <- peptide_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(9, 20),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_pep) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-
-
-pep_output <- layer_concatenate(inputs = c(pep_k1, 
-                                           pep_k3, 
-                                           pep_k5,
-                                           pep_k7,
-                                           pep_k9))
-
-## Build CDR3b layers
-CDR3b_input <- layer_input(shape = input_shape_CDR3b)
-
-CDR3b_k1 <- CDR3b_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(1, 21),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_CDR3b) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-CDR3b_k3 <- CDR3b_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(3, 21),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_CDR3b) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-CDR3b_k5 <- CDR3b_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(5, 21),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_CDR3b) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-CDR3b_k7 <- CDR3b_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(7, 21),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_CDR3b) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-CDR3b_k9 <- CDR3b_input %>% 
-      layer_conv_2d(filters = 16,
-                    kernel_size = c(9, 21),
-                    padding = "same",
-                    activation = 'relu',
-                    input_shape = input_shape_CDR3b) %>% 
-      layer_max_pooling_2d(pool_size = c(2, 2))
-
-CDR3b_output <- layer_concatenate(inputs = c(CDR3b_k1,
-                                             CDR3b_k3,
-                                             CDR3b_k5,
-                                             CDR3b_k7,
-                                             CDR3b_k9))
-
-## Concatenate final model architecture
-concatenated_model <- layer_concatenate(list(pep_output,
-                                             CDR3b_output),
-                                        axis = 1) %>% 
-      layer_flatten() %>% 
-      layer_dense(units = 100,
-                  activation = "relu")
-
-output_model <- concatenated_model %>% 
-      layer_dense(units = 1,
-                  activation = "sigmoid")
-
-cnn_model <- keras_model(
-      inputs = list(peptide_input, CDR3b_input),
-      outputs = output_model)
-
-## Compile model
-cnn_model %>% 
-      compile(loss = loss_func,
-              optimizer = optimizer_rmsprop(learning_rate = learn_rate),
-              metrics = "accuracy")
-
-## View model
-cnn_model %>% summary()
-
-
 # Train 20 models ---------------------------------------------------------
 meta_data <- list()
 model_dir <- "models/"
@@ -224,7 +108,123 @@ i <- 1
 for (outer_i in outer_folds) {
       for (inner_j in inner_folds) {
             
-            # Set partitions
+            # Set model architecture ------------------------------
+            
+            # Build peptide layers
+            peptide_input <- layer_input(shape = input_shape_pep)
+            
+            pep_k1 <- peptide_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(1, 20),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_pep) %>% 
+                  layer_max_pooling_2d(pool_size = 2)
+            pep_k3 <- peptide_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(3, 20),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_pep) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            pep_k5 <- peptide_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(5, 20),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_pep) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            pep_k7 <- peptide_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(7, 20),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_pep) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            pep_k9 <- peptide_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(9, 20),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_pep) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            
+            
+            pep_output <- layer_concatenate(inputs = c(pep_k1, 
+                                                       pep_k3, 
+                                                       pep_k5,
+                                                       pep_k7,
+                                                       pep_k9))
+            
+            # Build CDR3b layers
+            CDR3b_input <- layer_input(shape = input_shape_CDR3b)
+            
+            CDR3b_k1 <- CDR3b_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(1, 21),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_CDR3b) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            CDR3b_k3 <- CDR3b_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(3, 21),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_CDR3b) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            CDR3b_k5 <- CDR3b_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(5, 21),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_CDR3b) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            CDR3b_k7 <- CDR3b_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(7, 21),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_CDR3b) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            CDR3b_k9 <- CDR3b_input %>% 
+                  layer_conv_2d(filters = 16,
+                                kernel_size = c(9, 21),
+                                padding = "same",
+                                activation = 'relu',
+                                input_shape = input_shape_CDR3b) %>% 
+                  layer_max_pooling_2d(pool_size = c(2, 2))
+            
+            CDR3b_output <- layer_concatenate(inputs = c(CDR3b_k1,
+                                                         CDR3b_k3,
+                                                         CDR3b_k5,
+                                                         CDR3b_k7,
+                                                         CDR3b_k9))
+            
+            # Concatenate final model architecture
+            concatenated_model <- layer_concatenate(list(pep_output,
+                                                         CDR3b_output),
+                                                    axis = 1) %>% 
+                  layer_flatten() %>% 
+                  layer_dense(units = 100,
+                              activation = "relu")
+            
+            output_model <- concatenated_model %>% 
+                  layer_dense(units = 1,
+                              activation = "sigmoid")
+            
+            cnn_model <- keras_model(
+                  inputs = list(peptide_input, CDR3b_input),
+                  outputs = output_model)
+            
+            # Compile model
+            cnn_model %>% 
+                  compile(loss = loss_func,
+                          optimizer = optimizer_rmsprop(learning_rate = learn_rate),
+                          metrics = "accuracy")      
+            
+            
+            # Set partitions ----------------------------------------------
             test_partition <- as.data.frame(
                   partitions$splits[[outer_i]],
                   data = "assessment")
@@ -297,7 +297,8 @@ for (outer_i in outer_folds) {
             y_test <- test_partition %>% 
                   pull(Binding)
             
-            # Train model
+            
+            # Train model ---------------------------------------------
             history <- cnn_model %>% 
                   fit(x = list(X_train_pep, X_train_CDR3b),
                       y = y_train,
@@ -315,7 +316,8 @@ for (outer_i in outer_folds) {
             # Save model
             cnn_model %>% save_model_hdf5(filepath = model_file)
             
-            # Make predictions on test data
+            
+            # Make predictions on test data ---------------------------
             prediction <- cnn_model %>% 
                   predict(list(X_test_pep, 
                                X_test_CDR3b))
@@ -335,15 +337,17 @@ for (outer_i in outer_folds) {
                         )
             
             data_A0201 <- data_A0201 %>% 
-                  left_join(x = .,
-                            y = test_partition,
+                  left_join(y = test_partition,
                             by = c("CDR3b", "Peptide", "Binding", "CDR3b_size"))
             
       }
       
 }
 
-# Calculate mean predictions
+## View model
+#cnn_model %>% summary()
+
+## Calculate mean predictions
 data_A0201 <- data_A0201 %>% 
       mutate(pred_mdl_mean = select(., contains("pred_")) %>% 
                    rowMeans(na.rm = TRUE))
@@ -352,6 +356,52 @@ data_A0201 <- data_A0201 %>%
 
 # Model evaluation ---------------------------------------------------------
 
+## Define X and y final test data and encode amino acids
+X_test_final_pep <- data_A0201_test %>% 
+      pull(Peptide) %>% 
+      blosum_encoding(peptide = ., 
+                      blosum_matrix = blosum62) %>% 
+      array_reshape(., c(nrow(.), 9, 20, 1))
+
+X_test_final_CDR3b <- data_A0201_test %>%
+      pull(CDR3b)%>% 
+      blosum_encoding(peptide = .,
+                      blosum62_X) %>% 
+      array_reshape(., c(nrow(.), max_CDR3b, 21, 1))
+
+y_test_final <- data_A0201_test %>% 
+      pull(Binding)
+      
+
+model_files <- list.files(path = "models")
+all_models <- lapply(model_files, function(mdl_file) {
+      mdl_file <- str_c("models/", mdl_file)
+      load_model_hdf5(mdl_file)
+})
+
+a <- data_A0201_test
+## Evaluate 20 models on test data never seen before
+for (mdl_i in 1:20) {
+      
+      # Make predictions
+      predictions <- all_models[[mdl_i]] %>% 
+            predict(list(X_test_final_pep, 
+                         X_test_final_CDR3b))
+      # %>% 
+      #       as.array()
+      
+      # Add predictions to dataset
+      pred_mdl <- str_c("pred_mdl_", i)
+      # a <- a %>% 
+      #       bind_cols(predictions)
+      a <- a %>%
+             mutate(!!pred_mdl := predictions)
+}
+pred_mdl <- str_c("pred_", mdl_i)
+test_partition <- test_partition %>%
+      mutate(!!pred_mdl := as.numeric(
+            meta_data[[mdl_i]]$predictions_test)
+      )
 
 # Visualise data ----------------------------------------------------------
 
