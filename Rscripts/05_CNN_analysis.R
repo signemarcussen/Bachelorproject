083efdb23e194c8beba6d8c1cc9aa886950352d4
LucilleAugusta <s184250@student.dtu.dk>
2021-11-16 12:27:06 +0100

    confusion

--- Rscripts/05_CNN_analysis.R
+++ Rscripts/05_CNN_analysis.R
@@ -23,21 +23,35 @@ data_A0201_mdl_preds_test <- read_tsv(file = "data/04_data_A0201_mdl_preds_test.
 ROC_obj <- data_A0201_mdl_preds_test %>% 
       roc(response = Binding, 
           predictor = pred_mdl_mean, 
-          plot = TRUE)
+          plot = FALSE)
 AUC_obj <- auc(ROC_obj) %>% 
       round(digits = 2)
 
+# Get the class prediction w. a threshold of 0.5
+#y_pred <- as.numeric(data_A0201_mdl_preds_test$pred_mdl_mean >= 0.5)
+
+data_A0201_mdl_preds_test <- data_A0201_mdl_preds_test %>% 
+   mutate(y_pred = case_when(pred_mdl_mean >= 0.5 ~ 1,
+                             pred_mdl_mean < 0.5 ~ 0),
+          correct = case_when(Binding == y_pred ~ "Yes",
+                              Binding != y_pred ~ "No"))
+          # Binding = as.factor(Binding),
+          # pred_mdl_mean = as.factor(pred_mdl_mean))
 
 # Visualize data ----------------------------------------------------------
 
 ## ROC plot
 ROC_obj %>% ggroc(
       colour = 'steelblue', 
-                  size = 2) + 
-      ggtitle(paste0('ROC Curve ', '(AUC = ', AUC_obj, ')'))
+                  size = 1.5)  + 
+   geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), 
+                color="grey", linetype="dashed")+
+   ggtitle(paste0('ROC Curve ',
+                  '(AUC = ', AUC_obj, ')'))+
+   labs(x = "Specificity", 
+        y = "Sensitivity") +
+   theme_minimal()
 
-# Get the class prediction w. a threshold of 0.5
-# y_pred <- prediction %>%  as.numeric(prediction[,1] >= 0.5)
 
 ## Sensitivity / specificity plot
 # Getting the coordinates from the ROC-curve. 
@@ -64,8 +78,41 @@ ggplot(my_coords) +
                  color = 'black')+
       labs(x = "Cutoff for predicted probability", 
            y = "Sensitivity, Specificity") +
-      theme_minimal()
+      theme_minimal() +
+      theme(legend.position = "none")
+      
+## Confusion matrix
+CM <- table(data_A0201_mdl_preds_test$Binding, 
+            data_A0201_mdl_preds_test$y_pred) %>% as.matrix()
+
+#True positives 
+TP <- CM[2,2]
+
+#True negatives 
+TN <- CM[1,1]
+
+#False positives 
+FP <- CM[2,1]
+
+#False negatives 
+FN <- CM[1,2]
 
 
+data_A0201_mdl_preds_test %>% 
+   mutate(Binding = factor(Binding, levels= c(1,0)),
+          y_pred = factor(y_pred,levels=c(1,0))) %>% 
+   ggplot(.,mapping = aes(x = y_pred,
+                     y= Binding,
+                     fill=correct)) + 
+   geom_tile() +
+   #geom_text(aes(label= paste("",CM)))+
+   scale_x_discrete(position = "top")
+   theme_minimal()
+
+
+   scale_fill_gradient(low = "blue", high = "red")
+  
+plot_c
+ 
 # Write data --------------------------------------------------------------
 
